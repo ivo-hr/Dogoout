37e8bf2bbef86f04822bfdc68320b644bfc170a8
David Čadež <david.cadez89@gmail.com>
2023-11-01 19:07:24 +0000

    added components to the screens

--- app/src/main/java/com/example/dogoout/MainActivity.java
+++ app/src/main/java/com/example/dogoout/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register1);
+        setContentView(R.layout.activity_register4);
     }
 }
