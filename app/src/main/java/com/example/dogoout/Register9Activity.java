29c1cd8565a65bc72b2aa563be28226adba43011
David Čadež <david.cadez89@gmail.com>
2023-11-06 21:20:59 +0000

    added functionality for register 9 activity

--- app/src/main/java/com/example/dogoout/Register9Activity.java
+++ app/src/main/java/com/example/dogoout/Register9Activity.java
@@ -1,36 +1,140 @@
 package com.example.dogoout;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
+
+import java.util.ArrayList;
+
 public class Register9Activity extends AppCompatActivity {
+
+    // DECLARE COMPONENTS
     ImageView previousScreenBtn;
     Button nextScreenBtn;
+    ImageView imgVAdd1, imgVAdd2, imgVAdd3;
+    ImageView imgVDogImage1, imgVDogImage2, imgVDogImage3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register9);
 
+        // INITIALIZE COMPONENTS
+        imgVAdd1 = findViewById(R.id.imgVAdd1);
+        imgVAdd2 = findViewById(R.id.imgVAdd2);
+        imgVAdd3 = findViewById(R.id.imgVAdd3);
+
+        imgVDogImage1 = findViewById(R.id.imgVDogImage1);
+        imgVDogImage2 = findViewById(R.id.imgVDogImage2);
+        imgVDogImage3 = findViewById(R.id.imgVDogImage3);
+
         previousScreenBtn = findViewById(R.id.imgVBack);
+
         nextScreenBtn = findViewById(R.id.btnNext);
-        previousScreenBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
+
+        // SET ON CLICK LISTENERS
+        imgVAdd1.setOnClickListener(view -> {
+            handleImageInput(imgVDogImage1, imgVAdd1, 1);
         });
-        nextScreenBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Register10Activity.class);
-                startActivity(intent);
-            }
+        imgVAdd2.setOnClickListener(view -> {
+            handleImageInput(imgVDogImage2, imgVAdd2, 2);
         });
+        imgVAdd3.setOnClickListener(view -> {
+            handleImageInput(imgVDogImage3, imgVAdd3, 3);
+        });
+
+
+        previousScreenBtn.setOnClickListener(view -> finish());
+
+        nextScreenBtn.setOnClickListener(view -> {
+            // Create an array list of image views
+            ArrayList<ImageView> imageViews = new ArrayList<>();
+            imageViews.add(imgVDogImage1);
+            imageViews.add(imgVDogImage2);
+            imageViews.add(imgVDogImage3);
+
+            // Count the number of uploaded images
+            int uploadedImages = 0;
+            for (ImageView imageView : imageViews) {
+                if (imageView.getDrawable() != null)
+                    uploadedImages++;
+            }
+
+            // Check if the user uploaded at least 1 image1
+            if (uploadedImages < 1) {
+                // Display an error with alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(Register9Activity.this);
+                builder.setTitle("Error");
+                builder.setMessage("Please upload at least 1 image");
+                builder.setPositiveButton("OK", null);
+                builder.show();
+
+                return;
+            }
+
+            // TODO: collect data from the inputs
+
+            // Go to the next screen
+            Intent intent = new Intent(getApplicationContext(), Register10Activity.class);
+            startActivity(intent);
+        });
+    }
+
+    protected void handleImageInput(ImageView imageView, ImageView addImageView, int requestCode) {
+        if (imageView.getDrawable() == null) {
+            // Open the select image dialog if the image view is empty
+            ImagePicker
+                    .with(Register9Activity.this)
+                    .crop(4f, 5f)                            //Crop image,
+                    .compress(1024)                        //Final image size will be less than 0,5 MB
+                    .maxResultSize(1080, 1080)        //Final image resolution will be less than 1080 x 1080
+                    .start(requestCode);
+        } else {
+            // Remove the image from the image view and replace the add button with an add button
+            imageView.setImageResource(0);
+            addImageView.setImageResource(R.drawable.ic_add);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            switch (requestCode) {
+                case 1:
+                    // Set the image in ImageView and replace the add button with a remove button
+                    imgVDogImage1.setImageURI(uri);
+                    imgVAdd1.setImageResource(R.drawable.ic_remove);
+                    break;
+                case 2:
+                    // Set the image in ImageView and replace the add button with a remove button
+                    imgVDogImage2.setImageURI(uri);
+                    imgVAdd2.setImageResource(R.drawable.ic_remove);
+                    break;
+                case 3:
+                    // Set the image in ImageView and replace the add button with a remove button
+                    imgVDogImage3.setImageURI(uri);
+                    imgVAdd3.setImageResource(R.drawable.ic_remove);
+                    break;
+            }
+        } else {
+            // Display an error with alert dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(Register9Activity.this);
+            builder.setTitle("Error");
+            builder.setMessage("Something went wrong");
+            builder.setPositiveButton("OK", null);
+            builder.show();
+        }
     }
 }
