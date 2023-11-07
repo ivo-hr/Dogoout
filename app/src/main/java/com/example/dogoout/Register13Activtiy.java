abde7916f400751674d4194d80c5c1ee74b3717c
Tanguy Vandervelden <tanguy.vandervelden@student.vinci.be>
2023-11-07 02:06:44 +0000

    add : add email and password verifications

--- app/src/main/java/com/example/dogoout/Register13Activtiy.java
+++ app/src/main/java/com/example/dogoout/Register13Activtiy.java
@@ -1,16 +1,23 @@
 package com.example.dogoout;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Register13Activtiy extends AppCompatActivity {
     ImageView previousScreenBtn;
     Button nextScreenBtn;
+    EditText txtInEmail;
+    EditText txtInPassword;
+    EditText txtInConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,9 @@ public class Register13Activtiy extends AppCompatActivity {
 
         previousScreenBtn = findViewById(R.id.imgVBack);
         nextScreenBtn = findViewById(R.id.btnNext);
+        txtInEmail = findViewById(R.id.txtInEmail);
+        txtInPassword = findViewById(R.id.txtInPassword);
+        txtInConfirmPassword = findViewById(R.id.txtInConfirmPassword);
         previousScreenBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,9 +38,72 @@ public class Register13Activtiy extends AppCompatActivity {
         nextScreenBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Register14Activity.class);
-                startActivity(intent);
+                if (isValidTextInputEmail() && isValidTextInputPassword() && isValidTextInputConfirmPassword() && arePasswordTheSame()) {
+
+                    Intent intent = new Intent(getApplicationContext(), Register14Activity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
+
+    protected boolean isValidTextInputEmail() {
+        // Get the text input layout
+        TextInputLayout textInputLayout = findViewById(R.id.textInputLayout);
+        // Check if the edit text is empty
+        if (!txtInEmail.getText().toString().isEmpty()) {
+            // is its not empty its okay
+            textInputLayout.setErrorEnabled(false);
+            return true;
+        }
+        // set the error and error message
+        txtInEmail.getText().clear();
+        textInputLayout.setError("Please Fill Out this Field.");
+        return false;
+    }
+
+    protected boolean isValidTextInputPassword() {
+        // Get the text input layout
+        TextInputLayout textInputLayout = findViewById(R.id.textInputLayout1);
+        // Check if the edit text is empty
+        if (!txtInPassword.getText().toString().isEmpty()) {
+            // is its not empty its okay
+            textInputLayout.setErrorEnabled(false);
+            return true;
+        }
+        // set the error and error message
+        txtInPassword.getText().clear();
+        textInputLayout.setError("Please Fill Out this Field.");
+        return false;
+    }
+
+    protected boolean isValidTextInputConfirmPassword() {
+        // Get the text input layout
+        TextInputLayout textInputLayout = findViewById(R.id.textInputLayout2);
+        // Check if the edit text is empty
+        if (!txtInConfirmPassword.getText().toString().isEmpty()) {
+            // is its not empty its okay
+            textInputLayout.setErrorEnabled(false);
+            return true;
+        }
+        // set the error and error message
+        txtInConfirmPassword.getText().clear();
+        textInputLayout.setError("Please Fill Out this Field.");
+        return false;
+    }
+
+    protected boolean arePasswordTheSame() {
+
+        if (!txtInPassword.getText().toString().equals(txtInConfirmPassword.getText().toString())) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(Register13Activtiy.this);
+            builder.setTitle("Error");
+            builder.setMessage("Password and Confirm Password do not match. Please make sure both fields have the same value.");
+            builder.setPositiveButton("OK", null);
+            builder.show();
+            return false;
+        }
+        return true;
+
+    }
+
 }
