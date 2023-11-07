8a120fb37834a700bb4e44d4e537a2c38e450c9a
Tanguy Vandervelden <tanguy.vandervelden@student.vinci.be>
2023-11-07 01:50:23 +0000

    add : add prompts verification

--- app/src/main/java/com/example/dogoout/Register11Activity.java
+++ app/src/main/java/com/example/dogoout/Register11Activity.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
 
 public class Register11Activity extends AppCompatActivity {
     ImageView previousScreenBtn;
     Button nextScreenBtn;
+    AutoCompleteTextView promptTxtDog;
+    TextView textPromptDogAnswer;
+
+    ArrayList<String> arrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,16 @@ public class Register11Activity extends AppCompatActivity {
 
         previousScreenBtn = findViewById(R.id.imgVBack);
         nextScreenBtn = findViewById(R.id.btnNext);
+        textPromptDogAnswer = findViewById(R.id.txtInDogPromptAnswer);
+        promptTxtDog = findViewById(R.id.actxtDogPrompt);
+
+        arrayList.add("Prompt N°1");
+        arrayList.add("Prompt N°2");
+        arrayList.add("Prompt N°3");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, arrayList);
+        promptTxtDog.setAdapter(adapter);
+
         previousScreenBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,9 +49,41 @@ public class Register11Activity extends AppCompatActivity {
         nextScreenBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Register12Activity.class);
-                startActivity(intent);
+                if (isValidAnswerTxtPromptDog() && isValidTxtPromptDog()) {
+                    Intent intent = new Intent(getApplicationContext(), Register12Activity.class);
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    protected boolean isValidAnswerTxtPromptDog() {
+        // Get the text input layout
+        TextInputLayout textInputLayout = findViewById(R.id.textInputLayout);
+        // Check if the edit text is empty
+        if (!textPromptDogAnswer.getText().toString().isEmpty()) {
+            // is its not empty its okay
+            textInputLayout.setErrorEnabled(false);
+            return true;
+        }
+        // set the error and error message
+        textPromptDogAnswer.clearComposingText();
+        textInputLayout.setError("Please Fill Out this Field.");
+        return false;
+    }
+
+    protected boolean isValidTxtPromptDog() {
+        // Get the text input layout
+        TextInputLayout textInputLayout = findViewById(R.id.cbxLayout);
+        // Check if the edit text is empty
+        if (!promptTxtDog.getText().toString().isEmpty()) {
+            // is its not empty its okay
+            textInputLayout.setError(null);
+            return true;
+        }
+        // set the error and error message
+        promptTxtDog.setText("");
+        textInputLayout.setError("Please Fill Out this Field.");
+        return false;
     }
 }
--- app/src/main/java/com/example/dogoout/Register6Activity.java
+++ app/src/main/java/com/example/dogoout/Register6Activity.java
@@ -5,12 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
 
 public class Register6Activity extends AppCompatActivity {
     ImageView previousScreenBtn;
     Button nextScreenBtn;
+    AutoCompleteTextView promptTxt;
+    TextView answerTxtPrompt;
+
+    ArrayList<String> arrayList = new ArrayList<>();
+
+    //AutoCompleteTextView autoCompleteTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,18 @@ public class Register6Activity extends AppCompatActivity {
 
         previousScreenBtn = findViewById(R.id.imgVBack);
         nextScreenBtn = findViewById(R.id.btnNext);
+        promptTxt = findViewById(R.id.actxtPrompt);
+        answerTxtPrompt = findViewById(R.id.txtInPromptAnswer);
+
+        arrayList.add("Prompt N°1");
+        arrayList.add("Prompt N°2");
+        arrayList.add("Prompt N°3");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, arrayList);
+        promptTxt.setAdapter(adapter);
+
+
         previousScreenBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,9 +55,43 @@ public class Register6Activity extends AppCompatActivity {
         nextScreenBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Register7Activity.class);
-                startActivity(intent);
+                if (isValidAnswerTxtPrompt() && isValidTxtPrompt()) {
+                    Intent intent = new Intent(getApplicationContext(), Register7Activity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
+
+    protected boolean isValidAnswerTxtPrompt() {
+        // Get the text input layout
+        TextInputLayout textInputLayout = findViewById(R.id.textInputLayout);
+        // Check if the edit text is empty
+        if (!answerTxtPrompt.getText().toString().isEmpty()) {
+            // is its not empty its okay
+            textInputLayout.setErrorEnabled(false);
+            return true;
+        }
+        // set the error and error message
+        answerTxtPrompt.setText("");
+        textInputLayout.setError("Please Fill Out this Field.");
+        return false;
+    }
+
+    protected boolean isValidTxtPrompt() {
+        // Get the text input layout
+        TextInputLayout textInputLayout = findViewById(R.id.cbxLayout);
+        // Check if the edit text is empty
+        if (!promptTxt.getText().toString().isEmpty()) {
+            // is its not empty its okay
+            textInputLayout.setError(null);
+            return true;
+        }
+        // set the error and error message
+        promptTxt.setText("");
+        textInputLayout.setError("Please Fill Out this Field.");
+        return false;
+    }
+
+
 }
