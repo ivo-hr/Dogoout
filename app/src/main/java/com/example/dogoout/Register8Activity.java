df6df6fa90194d7f037c0d1e19d75500acde374c
Tanguy Vandervelden <tanguy.vandervelden@student.vinci.be>
2023-11-07 14:26:56 +0000

    add : add activity 8 verification

--- app/src/main/java/com/example/dogoout/Register8Activity.java
+++ app/src/main/java/com/example/dogoout/Register8Activity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
 
 public class Register8Activity extends AppCompatActivity {
     ImageView previousScreenBtn;
     Button nextScreenBtn;
+    AutoCompleteTextView acTxtVBreed;
+    EditText txtInDogName;
+    ArrayList<String> arrayListDogBreed = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,18 @@ public class Register8Activity extends AppCompatActivity {
 
         previousScreenBtn = findViewById(R.id.imgVBack);
         nextScreenBtn = findViewById(R.id.btnNext);
+        txtInDogName = findViewById(R.id.txtInDogName);
+        acTxtVBreed = findViewById(R.id.actxtVBreed);
+
+
+        arrayListDogBreed.add("Labrador");
+        arrayListDogBreed.add("Beagle");
+        arrayListDogBreed.add("Australian Shepherd");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, arrayListDogBreed);
+        acTxtVBreed.setAdapter(adapter);
+
+
         previousScreenBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,9 +50,41 @@ public class Register8Activity extends AppCompatActivity {
         nextScreenBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Register9Activity.class);
-                startActivity(intent);
+                if (isValidDogName() & isValidDogBreed()) {
+                    Intent intent = new Intent(getApplicationContext(), Register9Activity.class);
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    protected boolean isValidDogName() {
+        // Get the text input layout
+        TextInputLayout textInputLayout = findViewById(R.id.textInputLayout);
+        // Check if the edit text is empty
+        if (!txtInDogName.getText().toString().isEmpty()) {
+            // is its not empty its okay
+            textInputLayout.setError(null);
+            return true;
+        }
+        // set the error and error message
+        txtInDogName.setText("");
+        textInputLayout.setError("Please Fill Out this Field.");
+        return false;
+    }
+
+    protected boolean isValidDogBreed() {
+        // Get the text input layout
+        TextInputLayout textInputLayout = findViewById(R.id.cbxBreed);
+        // Check if the edit text is empty
+        if (!acTxtVBreed.getText().toString().isEmpty()) {
+            // is its not empty its okay
+            textInputLayout.setError(null);
+            return true;
+        }
+        // set the error and error message
+        acTxtVBreed.setText("");
+        textInputLayout.setError("Please Fill Out this Field.");
+        return false;
     }
 }
