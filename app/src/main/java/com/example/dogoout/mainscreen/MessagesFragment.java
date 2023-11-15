180edfb32b78d92715f8a9b1a33880348febd54d
David Čadež <david.cadez89@gmail.com>
2023-11-15 22:14:53 +0000

    added fragments for main activity

---
+++ app/src/main/java/com/example/dogoout/mainscreen/MessagesFragment.java
@@ -0,0 +1,27 @@
+package com.example.dogoout.mainscreen;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.dogoout.R;
+
+
+public class MessagesFragment extends Fragment {
+
+
+    public MessagesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_messages, container, false);
+    }
+}
