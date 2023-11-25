860533f5c253cf4a0b42134b7c067722462702c9
Jongsoo Han <jsoone24@gmail.com>
2023-11-25 02:44:48 +0000

    implemented fetching user data from firebase except dog, photos, preferences.

--- app/src/main/java/com/example/dogoout/login/LoginActivity.java
+++ app/src/main/java/com/example/dogoout/login/LoginActivity.java
@@ -13,7 +13,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dogoout.R;
 import com.example.dogoout.constants.Constants;
+import com.example.dogoout.domain.dog.Dog;
+import com.example.dogoout.domain.dog.DogBuilder;
+import com.example.dogoout.domain.preference.Preference;
 import com.example.dogoout.domain.user.User;
+import com.example.dogoout.domain.user.UserBuilder;
 import com.example.dogoout.domain.user.UserImpl;
 import com.example.dogoout.mainscreen.MainActivity;
 import com.example.dogoout.registration.Register1Activity;
@@ -22,6 +26,14 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -34,6 +46,8 @@ public class LoginActivity extends AppCompatActivity {
     Button loginButton;
     TextView registerTextView;
     FirebaseAuth firebaseAuth;
+    FirebaseUser currentUser;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +71,8 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(nextIntent);
         });
 
-        emailEditText.setText("david.cadez89@gmail.com");
-        passEditText.setText("Dublin123?");
+        emailEditText.setText("faience.sifter0v@icloud.com");
+        passEditText.setText("Dublin123!");
 
         //Set on click listener for login button
         loginButton.setOnClickListener(v -> {
@@ -70,25 +84,19 @@ public class LoginActivity extends AppCompatActivity {
                 builder.setMessage("Your email or password is incorrect");
                 builder.setPositiveButton("OK", null);
                 //Start main activity
-                firebaseAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passEditText.getText().toString())
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // TODO: get the user that is logged in (implement method getUserFromDatabase)
-                                    UserImpl user = (UserImpl) getUserFromDatabase();
-                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                    intent.putExtra(Constants.USER_TAG, user);
-                                    startActivity(intent);
-                                    finish();
+                firebaseAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passEditText.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // TODO: get the user that is logged in (implement method getUserFromDatabase)
+                            getUserFromDatabase();
+                        } else {
+                            builder.show();
+                            Log.w("LoginActivity", "signInWithEmail:failure", task.getException());
 
-                                } else {
-                                    builder.show();
-                                    Log.w("LoginActivity", "signInWithEmail:failure", task.getException());
-
-                                }
-                            }
-                        });
+                        }
+                    }
+                });
             }
         });
     }
@@ -96,6 +104,71 @@ public class LoginActivity extends AppCompatActivity {
 
     public User getUserFromDatabase() {
         // TODO: get the user that is logged in
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        // Fetch user data from Firebase Data Store
+        fStore = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = fStore.collection("users").document(currentUser.getUid());
+
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        // Get user data from the document
+                        String birthDate = document.getString("birthDate");
+                        String country = document.getString("country");
+                        String description = document.getString("description");
+
+                        //ArrayList<Dog> dogs = document.get("dogs");
+                        String email = document.getString("email");
+                        String firstname = document.getString("firstname");
+                        String gender = document.getString("gender");
+
+                        //ArrayList<String> photos = document.get("photos");
+                        String prompt = document.getString("prompt");
+                        String promptAnswer = document.getString("promptAnswer");
+                        String surname = document.getString("surname");
+
+                        /*
+                        userPreference = document.getString("userPreference");
+                        Preference preference = new Preference();
+                        preference.setSexPreference(Constants.PREF_OTHER);
+                        preference.setDogBreedPreference(Constants.PREF_BREED_ALL);
+                        preference.setDogOwnerPreference(Constants.PREF_DOG_OWNERS);
+                        preference.setMinAge(18);
+                        preference.setMaxAge(29);
+                         */
+
+                        UserBuilder userBuilder = new UserBuilder().withBirthDate(LocalDate.parse(birthDate))
+                                .withCountry(country)
+                                .withDescription(description)
+                                //.withDog(dogBuilder.build())
+                                .withEmail(email)
+                                .withFirstname(firstname)
+                                .withGender(gender)
+                                //.withPhotosUser(photos)
+                                .withSurname(surname)
+                                .withPrompt(prompt)
+                                .withPromptAnswer(promptAnswer);
+                                //.withPreference(userPreference);
+
+                        UserImpl user = userBuilder.build();
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.putExtra(Constants.USER_TAG, user);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        // Handle user not found
+                    }
+                } else {
+                    // Handle error
+                }
+            }
+        });
+
         return null;
     }
 
