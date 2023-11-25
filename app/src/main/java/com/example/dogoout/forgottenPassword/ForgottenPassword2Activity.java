a17d31518573950108831143ba4e06ea61268358
David Čadež <david.cadez89@gmail.com>
2023-11-25 13:19:17 +0000

    added layout for password reset feature

---
+++ app/src/main/java/com/example/dogoout/forgottenPassword/ForgottenPassword2Activity.java
@@ -0,0 +1,16 @@
+package com.example.dogoout.forgottenPassword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dogoout.R;
+
+public class ForgottenPassword2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgotten_password2);
+    }
+}
